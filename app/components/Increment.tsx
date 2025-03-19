04615e29e2634b943e0444b1a8772cdf6d6bfaeb
skytech2510 <skytech2510@gmail.com>
2025-03-19 18:29:22 +0800

    feat: add Increment

--- app/components/Increment.tsx
+++ app/components/Increment.tsx
@@ -1,7 +1,23 @@
 import Image from 'next/image';
 import React from 'react';
+import { getDayInfo } from '../utiles';
+import AT_RISK from "@/public/assets/at_risk.svg"
+import INCOMPLETED from "@/public/assets/incompleted.svg"
+import INCOMPLETED_TODAY from "@/public/assets/incompleted_today.svg"
+import SAVED from "@/public/assets/saved.svg"
+import COMPLETED from "@/public/assets/completed.svg" // If different, replace with actual completed icon  
+
+type Status = "INCOMPLETE" | "COMPLETED" | "SAVED" | "AT_RISK";
+
+const STATUS_IMAGES: Record<Status, any> = {
+    "AT_RISK": AT_RISK,
+    "INCOMPLETE": INCOMPLETED,
+    "SAVED": SAVED,
+    "COMPLETED": COMPLETED
+};
+
 interface IncrementPropsType {
-    status: "INCOMPLETE" | "COMPLETED" | "SAVED" | "AT_RISK",
+    status: Status,
     date: Date,
     activities: number
 }
@@ -12,8 +28,16 @@ export const Increment: React.FC<IncrementPropsType> = ({
     activities
 }) => {
     return (
-        <div className=' flex flex-col gap-2'>
-            <Image width={24} height={24} alt='' src={"/assets/Day.png"}></Image>
+        <div className='flex flex-col gap-2 items-center px-[14.29px] pb-2 pt-1 shadow-[0_1.5px_0px_0px_#E6E6E6]'>
+            <Image
+                width={24}
+                height={24}
+                alt={`${status} status`}
+                src={STATUS_IMAGES[status]}
+            />
+            <div className='text-[#A2A2A2] text-[12px]'>
+                {getDayInfo(date).shortName.toUpperCase()}
+            </div>
         </div>
     )
-}
+}  
